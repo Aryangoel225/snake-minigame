f4c492a27313c62f6a13f2de2a69e5dff8e9c69f
Aryangoel225 <aryangoel225@gmail.com>
2025-02-02 19:32:04 +0000

    kk

---
+++ index.js
@@ -0,0 +1,17 @@
+const gameBoard = document.getElementById("gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.getElementById("score");
+const resetBtn = document.getElementById("resetBtn");
+const gameWidth = gameBoard.gameWidth;
+const gameHeight =  gameBoard.gameHeight;
+const snakeColor = "lightgreen";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+
+
